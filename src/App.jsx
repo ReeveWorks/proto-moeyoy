4990be02c3dcb10822eb902a946d371d2cc011f7
EndPlay <128982259+ReeveWorks@users.noreply.github.com>
2026-02-19 09:23:47 +0800

    v0.1 Style Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -19,10 +18,10 @@ function App() {
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          Count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Edit src/App.jsx and save to test HMR
         </p>
       </div>
       <p className="read-the-docs">
